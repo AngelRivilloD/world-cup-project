0b7d8204dd582ce7abfa09431eb8a0646c65791b
Angel Antonio Rivillo Diaz <antorivillo@gmail.com>
2023-02-16 12:23:20 +0100

    filereplacement and environment gitignore not

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api-football-v1.p.rapidapi.com/v3',
+    apiKey: '8a95ee2023msh1c3cd1b258ce8f4p11fdfcjsn4930b12f6300',
+    apiHost: 'api-football-v1.p.rapidapi.com',
+    SECRET_KEY: 'impossible-is-nothing2023'
+};
